8aff8cd1f03a56ca1240df24625c3d33ef90e1e0
Jamilu Salisu <jamilusalis@gmail.com>
2022-02-28 18:07:37 +0100

    add db file

--- database/kadpolybusbooking_db.sql
+++ database/kadpolybusbooking_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2020 at 04:32 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Feb 28, 2022 at 06:07 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bus_booking`
+-- Database: `kadpolybusbooking_db`
 --
 
 -- --------------------------------------------------------
@@ -37,15 +37,6 @@ CREATE TABLE `booked` (
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `booked`
---
-
-INSERT INTO `booked` (`id`, `schedule_id`, `ref_no`, `name`, `qty`, `status`, `date_updated`) VALUES
-(1, 1, '202009091727', 'John Smith', 1, 1, '2020-09-09 10:29:44'),
-(2, 1, '202009091626', 'Sample', 2, 0, '2020-09-09 09:34:28'),
-(3, 1, '202009099953', 'asdasd asdasd', 27, 0, '2020-09-09 09:53:09');
-
 -- --------------------------------------------------------
 
 --
@@ -59,13 +50,6 @@ CREATE TABLE `bus` (
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0 = inactive, 1 = active',
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `bus`
---
-
-INSERT INTO `bus` (`id`, `name`, `bus_number`, `status`, `date_updated`) VALUES
-(3, 'Economy', '5001', 1, '2020-09-08 13:54:42');
 
 -- --------------------------------------------------------
 
@@ -81,14 +65,6 @@ CREATE TABLE `location` (
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '0= inactive , 1= active',
   `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `location`
---
-
-INSERT INTO `location` (`id`, `terminal_name`, `city`, `state`, `status`, `date_updated`) VALUES
-(1, 'Sample Terminal Name', 'Sample City', 'Sample', 1, '2020-09-08 14:23:36'),
-(2, 'South Sample Terminal', 'South City', 'Sample', 1, '2020-09-08 14:33:04');
 
 -- --------------------------------------------------------
 
@@ -108,14 +84,6 @@ CREATE TABLE `schedule_list` (
   `price` text NOT NULL,
   `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `schedule_list`
---
-
-INSERT INTO `schedule_list` (`id`, `bus_id`, `from_location`, `to_location`, `departure_time`, `eta`, `status`, `availability`, `price`, `date_updated`) VALUES
-(1, 3, 1, 1, '2020-09-11 16:00:00', '2020-09-12 02:00:00', 1, 30, '250', '2020-09-08 07:49:57'),
-(2, 3, 2, 1, '2020-09-12 02:45:00', '2020-09-12 05:00:00', 1, 30, '250', '2020-09-08 07:37:52');
 
 -- --------------------------------------------------------
 
@@ -138,8 +106,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `user_type`, `username`, `password`, `status`, `date_updated`) VALUES
-(1, 'Administrator', 1, 'admin', 'admin123', 1, '2020-09-08 16:42:28'),
-(2, 'John Smith', 1, 'jsmth', 'admin123', 1, '2020-09-08 16:13:53');
+(1, 'Administrator', 1, 'admin', 'admin', 1, '2022-02-28 16:42:28');
 
 --
 -- Indexes for dumped tables
@@ -183,31 +150,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `booked`
 --
 ALTER TABLE `booked`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `bus`
 --
 ALTER TABLE `bus`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `location`
 --
 ALTER TABLE `location`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `schedule_list`
 --
 ALTER TABLE `schedule_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,4 @@
 <?php
-$conn= new mysqli('localhost','root','','bus_booking')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','kadpolybusbooking_db')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
